17619a025d32fb64bcbaad5003be33a5d849e28b
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-16 20:24:32 +0800

    Add sample footer

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -23,12 +23,36 @@ const AppLayout: React.FC<{
         description={description || metadata.description}
         siteTitle={metadata.title}
       />
-      <Header
-        isActive={isActive}
-        toggleMenu={handler.toggleMenu}
-        headerImage={metadata.headerImage}
-      />
-      {children}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}>
+        <Header
+          isActive={isActive}
+          toggleMenu={handler.toggleMenu}
+          headerImage={metadata.headerImage}
+        />
+        <section className="section" style={{ flex: 1 }}>
+          <div className="container">{children}</div>
+        </section>
+        <footer className="footer">
+          <div className="content has-text-centered">
+            <p>
+              <strong>Bulma</strong> by{" "}
+              <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
+              licensed
+              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+              The website content is licensed{" "}
+              <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+                CC BY NC SA 4.0
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
+      </div>
     </>
   )
 }
