0909332e507cebd99884a6903d8f5bfbb6893448
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-16 15:09:35 +0800

    Configure bulma theme Pulse from Bulmaswatch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage: React.FC<{}> = () => {
           <p className="content">Cool content. Using Bulma!</p>
         </div>
         <div className="column is-four-fifths">
-          <h2 className="title is-2">Level 2 heading</h2>
+          <h2 className="title is-2 has-text-warning">Level 2 heading</h2>
           <p className="content">This column is cool too!</p>
         </div>
       </div>
