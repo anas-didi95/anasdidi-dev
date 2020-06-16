2a56c0ae3dd821420a112330cb93a113dc689b45
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-16 16:44:16 +0800

    Refactor component

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import GatsbyImage, { FixedObject } from "gatsby-image"
+import { Link } from "gatsby"
+
+const Header: React.FC<{
+  headerImage?: FixedObject
+  isActive: boolean
+  toggleMenu: any
+}> = ({ headerImage, isActive, toggleMenu }) => (
+  <nav
+    className="navbar is-dark"
+    role="navigation"
+    aria-label="main navigation">
+    <div className="navbar-brand">
+      <Link className="navbar-item" to="/">
+        <GatsbyImage fixed={headerImage} />
+      </Link>
+      <a
+        role="button"
+        className={`navbar-burger ${isActive ? "is-active" : ""}`}
+        aria-label="menu"
+        aria-expanded="false"
+        onClick={toggleMenu}>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+      <div className="navbar-start">
+        <Link className="navbar-item" to="/">
+          Home
+        </Link>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import GatsbyImage, { FixedObject } from "gatsby-image"
+import { FixedObject } from "gatsby-image"
 import { IndexPageQueryQuery } from "../graphqlTypes"
 import { oc } from "ts-optchain"
+import Header from "../components/Header"
 
 const IndexPage: React.FC<{}> = () => {
   const [isActive, setActive] = useState(false)
@@ -30,65 +31,13 @@ const IndexPage: React.FC<{}> = () => {
   }
 
   return (
-    <nav
-      className="navbar is-dark"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <GatsbyImage
-            fixed={
-              (oc(data).file.childImageSharp.fixed() as FixedObject) || null
-            }
-          />
-        </a>
-        <a
-          role="button"
-          className={`navbar-burger ${handler.getMenuActive()}`}
-          aria-label="menu"
-          aria-expanded="false"
-          onClick={handler.toggleMenu}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div
-        id="navbarBasicExample"
-        className={`navbar-menu ${handler.getMenuActive()}`}
-      >
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-
-          <a className="navbar-item">Documentation</a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">About</a>
-              <a className="navbar-item">Jobs</a>
-              <a className="navbar-item">Contact</a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item">Report an issue</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <>
+      <Header
+        isActive={isActive}
+        toggleMenu={handler.toggleMenu}
+        headerImage={oc(data).file.childImageSharp.fixed() as FixedObject}
+      />
+    </>
   )
 }
 
