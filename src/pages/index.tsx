fb97ff89c24483c5bdb5d4ea2fbecccb10d90ac4
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-16 15:43:12 +0800

    Add sample header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,63 @@ import React from "react"
 
 const IndexPage: React.FC<{}> = () => {
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <h2 className="title is-2 has-text-primary">Level 2 heading</h2>
-          <p className="content">Cool content. Using Bulma!</p>
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand m-2">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+
+          <a className="navbar-item">Documentation</a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
+          </div>
         </div>
-        <div className="column is-four-fifths">
-          <h2 className="title is-2 has-text-warning">Level 2 heading</h2>
-          <p className="content">This column is cool too!</p>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   )
 }
 
