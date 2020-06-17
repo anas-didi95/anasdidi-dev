4e62fb77c0096dfbecc59328e67dde2c9bf1c450
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-17 12:23:51 +0800

    Extract component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,33 @@
 import React from "react"
 import AppLayout from "../layouts/AppLayout"
+import BlogList from "../components/BlogList"
+import * as Types from "../utils/types"
 
 const IndexPage: React.FC<{}> = () => {
+  const blogList: Types.Blog[] = [
+    {
+      title: "Introduction to Algorithm: Insertion Sort",
+      author: "Anas Juwaidi",
+      date: "June 17, 2020",
+      description:
+        "Introduction to Insertion Sort, analysis and implementation on various programming languages.",
+      tags: ["algorithm"],
+    },
+    {
+      title: "Hello World",
+      author: "Anas Juwaidi",
+      date: "June 17, 2020",
+      description: "Sample page to display Markdown rendering.",
+      tags: ["markdown"],
+    },
+  ]
+
   return (
     <AppLayout description="Home page" title="Home">
       <div className="columns">
         <div className="column" />
         <div className="column is-6">
-          <div className="box px-6 py-5">
-            <p className="title is-4 mb-3">
-              Introduction to Algorithm: Insertion Sort
-            </p>
-            <div className="mb-5">
-              <div className="columns is-mobile">
-                <div className="column">
-                  <p className="subtitle is-6 is-italic">Anas Juwaidi</p>
-                </div>
-                <div className="column" />
-                <div className="column">
-                  <p className="has-text-right subtitle is-6 is-italic">
-                    June 17, 2020
-                  </p>
-                </div>
-              </div>
-            </div>
-            <p className="mb-5">
-              Introduction to Insertion Sort, analysis and implementation on
-              various programming languages.
-            </p>
-            <br />
-            <div className="tags are-medium">
-              <span className="tag is-primary mr-2">Black</span>
-            </div>
-          </div>
-          <div className="box px-6 py-5">
-            <p className="title is-4 mb-3">Hello World</p>
-            <div className="mb-5">
-              <div className="columns is-mobile">
-                <div className="column">
-                  <p className="subtitle is-6 is-italic">Anas Juwaidi</p>
-                </div>
-                <div className="column" />
-                <div className="column">
-                  <p className="has-text-right subtitle is-6 is-italic">
-                    June 17, 2020
-                  </p>
-                </div>
-              </div>
-            </div>
-            <p className="mb-5">Sample page to display Markdown rendering.</p>
-            <br />
-            <div className="tags are-medium">
-              <span className="tag is-primary mr-2">Black</span>
-            </div>
-          </div>
+          <BlogList blogList={blogList} />
         </div>
         <div className="column" />
       </div>
