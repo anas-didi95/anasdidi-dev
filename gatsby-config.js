e9ded4d4d78481947784dc0f1b892d0d05b21c42
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-19 11:36:49 +0800

    Refactor gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-extract-schema`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,9 +34,6 @@ module.exports = {
         name: `aboutMe`,
       },
     },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -37,8 +41,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -51,8 +53,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-extract-schema`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
