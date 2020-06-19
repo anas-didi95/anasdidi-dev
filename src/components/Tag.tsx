ed681b1208f2448c2693c0ed88530413fb0bbe3b
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-19 15:06:20 +0800

    Add hash for tag

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Maybe } from "../graphqlTypes"
 
 const Tag: React.FC<{ value: Maybe<string> }> = ({ value }) => (
-  <span className="tag is-primary mr-2">{value}</span>
+  <span className="tag is-primary mr-2">#{value}</span>
 )
 
 export default Tag
