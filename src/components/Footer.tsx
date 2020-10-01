0fd1bf2c903281068adc58d74e057e684868ab7d
Anas Juwaidi <anas.didi95@gmail.com>
2020-10-01 16:40:29 +0800

    Amend text to increase visibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,33 +6,33 @@ const Footer: React.FC<{ email: string; github: string; linkedin: string }> = ({
   github,
   linkedin,
 }) => (
-  <footer className="footer">
-    <div className="content has-text-centered">
-      <div className="columns">
-        <div className="column" />
-        <div className="buttons column">
-          <SocialLink icon={<Icon type="email" />} link={`mailto:${email}`} />
-          <SocialLink icon={<Icon type="github" />} link={github} />
-          <SocialLink icon={<Icon type="linkedin" />} link={linkedin} />
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <div className="columns">
+          <div className="column" />
+          <div className="buttons column">
+            <SocialLink icon={<Icon type="email" />} link={`mailto:${email}`} />
+            <SocialLink icon={<Icon type="github" />} link={github} />
+            <SocialLink icon={<Icon type="linkedin" />} link={linkedin} />
+          </div>
+          <div className="column" />
         </div>
-        <div className="column" />
+        <p>All rights reserved &copy; {new Date().getFullYear()}</p>
+        <p>
+          Built with <a className="has-text-primary has-text-weight-bold" href="https://bulma.io/">Bulma</a> and{" "}
+          <a className="has-text-primary has-text-weight-bold" href="https://www.gatsbyjs.org/">Gatsby</a>{" "}
+        </p>
       </div>
-      <p>All rights reserved &copy; {new Date().getFullYear()}</p>
-      <p>
-        Built with <a href="https://bulma.io/">Bulma</a> and{" "}
-        <a href="https://www.gatsbyjs.org/">Gatsby</a>{" "}
-      </p>
-    </div>
-  </footer>
-)
+    </footer>
+  )
 
 const SocialLink: React.FC<{ icon: ReactNode; link: string }> = ({
   icon,
   link,
 }) => (
-  <a className="button is-text" target="_blank" href={link}>
-    <span className="is-size-3">{icon}</span>
-  </a>
-)
+    <a className="button is-text" target="_blank" href={link}>
+      <span className="is-size-3">{icon}</span>
+    </a>
+  )
 
 export default Footer
