3ee130e1be87acac6dc265f105e5de2d537d18e4
Anas Juwaidi <anas.didi95@gmail.com>
2021-04-30 17:27:59 +0800

    Get latest starter from github

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,57 +1,13 @@
 import React from "react"
-import AppLayout from "../layouts/AppLayout"
-import Box from "../components/Box"
-import ResponsiveBreakpoint from "../components/ResponsiveBreakpoint"
-import { useStaticQuery, graphql, Link } from "gatsby"
-import { Error404PageQuery } from "../graphqlTypes"
-import GatsbyImage, { FixedObject } from "gatsby-image"
-import { oc } from "ts-optchain"
-import Button from "../components/Button"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-const Error404Page: React.FC<{}> = () => {
-  const data: Error404PageQuery = useStaticQuery(graphql`
-    query Error404Page {
-      icon: file(absolutePath: { regex: "/images/warning.png/" }) {
-        childImageSharp {
-          fixed(width: 96, height: 96) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
+const NotFoundPage: React.FC = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Layout>
+)
 
-  return (
-    <AppLayout title="Error 404">
-      <ResponsiveBreakpoint />
-      <div className="columns">
-        <div className="column" />
-        <div className="column is-6">
-          <Box>
-            <div className="columns">
-              <div className="column is-3 has-text-centered">
-                <GatsbyImage
-                  fixed={oc(data).icon.childImageSharp.fixed() as FixedObject}
-                />
-              </div>
-              <div className="column">
-                <p className="title">Page Not Found!</p>
-                <p className="content">
-                  Looks like you've followed a broken link or entered a URL that
-                  doesn't exist on this site.
-                </p>
-                <div className="buttons">
-                  <Button type="link" value="Return to Home" link="/" />
-                </div>
-              </div>
-            </div>
-          </Box>
-        </div>
-        <div className="column" />
-      </div>
-      <ResponsiveBreakpoint />
-    </AppLayout>
-  )
-}
-
-export default Error404Page
+export default NotFoundPage
