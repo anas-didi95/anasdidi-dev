f69377905df0beda7af144b29860a8e5d6370e5f
Anas Juwaidi <anas.didi95@gmail.com>
2021-05-06 10:22:59 +0800

    Implemenet reducer actions to handle pagination

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -7,8 +7,18 @@ import { TArticle } from "../utils/types"
 
 interface IArticleList {
   articles: TArticle[]
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  handleNextPage: () => void
+  handlePreviousPage: () => void
 }
-const ArticleList: React.FC<IArticleList> = ({ articles }) => (
+const ArticleList: React.FC<IArticleList> = ({
+  articles,
+  hasNextPage,
+  hasPreviousPage,
+  handleNextPage,
+  handlePreviousPage,
+}) => (
   <>
     <div className="columns is-multiline">
       {articles.map((article, i) => (
@@ -18,7 +28,10 @@ const ArticleList: React.FC<IArticleList> = ({ articles }) => (
       ))}
     </div>
     <nav className="pagination" role="navigation" aria-label="pagination">
-      <button className="pagination-previous button is-dark" disabled>
+      <button
+        className="pagination-previous button is-dark"
+        onClick={handlePreviousPage}
+        disabled={!hasPreviousPage}>
         <span
           className="mr-2"
           style={{ display: "flex", alignItems: "center" }}>
@@ -26,7 +39,10 @@ const ArticleList: React.FC<IArticleList> = ({ articles }) => (
         </span>
         <span>Previous</span>
       </button>
-      <button className="pagination-next button is-dark">
+      <button
+        className="pagination-next button is-dark"
+        onClick={handleNextPage}
+        disabled={!hasNextPage}>
         <span>Next page</span>
         <span
           className="ml-2"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Reducer, useMemo, useReducer } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import AppLayout from "../layouts/AppLayout"
 import ArticleList from "../components/ArticleList"
@@ -30,22 +30,39 @@ const IndexPage: React.FC<{}> = () => {
       }
     }
   `)
+  const [state, dispatch] = useReducerAction(data.articles.edges.length, 5)
 
-  const articles: TArticle[] = data.articles.edges.map((edge) => ({
-    author: edge.node.frontmatter?.author ?? "",
-    date: edge.node.frontmatter?.date ?? "",
-    description: edge.node.frontmatter?.description ?? "",
-    excerpt: edge.node.excerpt ?? "",
-    slug: edge.node.fields?.slug ?? "",
-    tags: edge.node.frontmatter?.tags ?? [],
-    title: edge.node.frontmatter?.title ?? "",
-  }))
+  const articles: TArticle[] = useMemo(
+    () =>
+      data.articles.edges
+        .map((edge) => ({
+          author: edge.node.frontmatter?.author ?? "",
+          date: edge.node.frontmatter?.date ?? "",
+          description: edge.node.frontmatter?.description ?? "",
+          excerpt: edge.node.excerpt ?? "",
+          slug: edge.node.fields?.slug ?? "",
+          tags: edge.node.frontmatter?.tags ?? [],
+          title: edge.node.frontmatter?.title ?? "",
+        }))
+        .slice(
+          (state.currentPage - 1) * state.articlesPerPage,
+          (state.currentPage - 1) * state.articlesPerPage +
+            state.articlesPerPage
+        ),
+    [state.currentPage]
+  )
 
   return (
     <AppLayout title="Home">
       <div className="columns is-centered">
         <div className="column is-10">
-          <ArticleList articles={articles} />
+          <ArticleList
+            articles={articles}
+            handleNextPage={() => dispatch({ type: "NEXT_PAGE" })}
+            handlePreviousPage={() => dispatch({ type: "PREVIOUS_PAGE" })}
+            hasNextPage={state.hasNextPage}
+            hasPreviousPage={state.hasPreviousPage}
+          />
         </div>
       </div>
     </AppLayout>
@@ -53,3 +70,48 @@ const IndexPage: React.FC<{}> = () => {
 }
 
 export default IndexPage
+
+type TState = {
+  currentPage: number
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  articlesPerPage: number
+  totalPages: number
+}
+type TAction = { type: "NEXT_PAGE" } | { type: "PREVIOUS_PAGE" }
+const reducer: Reducer<TState, TAction> = (state, action) => {
+  const { type } = action
+  if (type === "NEXT_PAGE") {
+    const { currentPage, totalPages } = state
+    return {
+      ...state,
+      currentPage: currentPage + 1,
+      hasNextPage: currentPage + 1 !== totalPages,
+      hasPreviousPage: true,
+    }
+  } else if (type === "PREVIOUS_PAGE") {
+    let { currentPage } = state
+    return {
+      ...state,
+      currentPage: currentPage - 1,
+      hasNextPage: true,
+      hasPreviousPage: currentPage - 1 !== 1,
+    }
+  } else {
+    throw new Error(`Action Type not defined! ${type}`)
+  }
+}
+const initialState: TState = {
+  currentPage: 1,
+  hasNextPage: true,
+  hasPreviousPage: false,
+  articlesPerPage: -1,
+  totalPages: -1,
+}
+const useReducerAction = (totalArticles: number, articlesPerPage: number) =>
+  useReducer<Reducer<TState, TAction>>(reducer, {
+    ...initialState,
+    hasNextPage: totalArticles / articlesPerPage !== 1,
+    articlesPerPage,
+    totalPages: Math.ceil(totalArticles / articlesPerPage),
+  })
