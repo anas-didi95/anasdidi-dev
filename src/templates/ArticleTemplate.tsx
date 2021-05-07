bcc784b91f14dfd2145a78568d67fd43e673faac
Anas Juwaidi <anas.didi95@gmail.com>
2021-05-07 15:44:28 +0800

    Run formatter and build;

--- src/templates/ArticleTemplate.tsx
+++ src/templates/ArticleTemplate.tsx
@@ -9,7 +9,7 @@ import { ArticleTemplateQuery } from "../../graphql-types"
 
 interface IArticleTemplate {
   data: ArticleTemplateQuery
-  pageContext: { next?: TArticleNode, previous?: TArticleNode }
+  pageContext: { next?: TArticleNode; previous?: TArticleNode }
 }
 const ArticleTemplate: React.FC<IArticleTemplate> = ({ data, pageContext }) => {
   return (
@@ -31,9 +31,10 @@ const ArticleTemplate: React.FC<IArticleTemplate> = ({ data, pageContext }) => {
               <div
                 className="tags are-medium"
                 style={{ justifyContent: "center" }}>
-                {!!data.markdownRemark?.frontmatter?.tags && data.markdownRemark.frontmatter.tags.map((tag, i) => (
-                  <Tag key={`tag${i}`} value={tag ?? ""} isHighlighted />
-                ))}
+                {!!data.markdownRemark?.frontmatter?.tags &&
+                  data.markdownRemark.frontmatter.tags.map((tag, i) => (
+                    <Tag key={`tag${i}`} value={tag ?? ""} isHighlighted />
+                  ))}
               </div>
             </div>
           </Box>
@@ -44,7 +45,7 @@ const ArticleTemplate: React.FC<IArticleTemplate> = ({ data, pageContext }) => {
           <div
             className="content"
             dangerouslySetInnerHTML={{
-              __html: data.markdownRemark?.html ?? ""
+              __html: data.markdownRemark?.html ?? "",
             }}
           />
         </div>
@@ -54,7 +55,10 @@ const ArticleTemplate: React.FC<IArticleTemplate> = ({ data, pageContext }) => {
   )
 }
 
-const _Pagination: React.FC<{ next?: TArticleNode, previous?: TArticleNode }> = ({ next, previous }) => (
+const _Pagination: React.FC<{
+  next?: TArticleNode
+  previous?: TArticleNode
+}> = ({ next, previous }) => (
   <div className="columns is-centered">
     <div className="column is-7">
       {next && (
@@ -62,10 +66,13 @@ const _Pagination: React.FC<{ next?: TArticleNode, previous?: TArticleNode }> = 
           <Link
             className="has-text-primary has-text-weight-bold"
             to={next.fields.slug}>
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
-              <span className="mr-2">
-                {next.frontmatter.title}
-              </span>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}>
+              <span className="mr-2">{next.frontmatter.title}</span>
               <Icon type="next" />
             </div>
           </Link>
@@ -79,9 +86,7 @@ const _Pagination: React.FC<{ next?: TArticleNode, previous?: TArticleNode }> = 
             to={previous.fields.slug}>
             <div style={{ display: "flex", alignItems: "center" }}>
               <Icon type="previous" />
-              <span className="ml-2">
-                {previous.frontmatter.title}
-              </span>
+              <span className="ml-2">{previous.frontmatter.title}</span>
             </div>
           </Link>
         </div>
