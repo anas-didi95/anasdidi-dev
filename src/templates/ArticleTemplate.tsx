de7827d31d68095247a8594007c2fc9dc88593d3
Anas Juwaidi <anas.didi95@gmail.com>
2021-05-07 09:00:42 +0800

    Implement article query values to template

--- src/templates/ArticleTemplate.tsx
+++ src/templates/ArticleTemplate.tsx
@@ -4,6 +4,7 @@ import AppLayout from "../layouts/AppLayout"
 import Box from "../components/Box"
 import Tag from "../components/Tag"
 import ResponsiveBreakpoint from "../components/ResponsiveBreakpoint"
+import { ArticleTemplateQuery } from "../../graphql-types"
 //import { BlogTemplateQuery } from "../graphqlTypes"
 //import { oc } from "ts-optchain"
 //import BlogPagination from "../components/BlogPagination"
@@ -27,73 +28,71 @@ type TPageContext = {
   }
 }
 
-const BlogTemplate: React.FC<{}> = (props) => (
-  <AppLayout
-    title=""
-    description="">
-    <div className="columns">
-      <div className="column" />
-      <div className="column is-8">
-        <Box>
-          <div className="has-text-centered">
-            <p className="title is-3">
-              {/*oc(data).markdownRemark.frontmatter.title("")*/}
-            </p>
-            <p className="subtitle is-6">
-              <span className="is-italic">
-                {/*oc(data).markdownRemark.frontmatter.date("")*/}
-              </span>
-            </p>
-            <div
-              className="tags are-medium"
-              style={{ justifyContent: "center" }}>
-              {/*oc(data)
-                .markdownRemark.frontmatter.tags([])
-                .map((tag, i) => (
-                  <Tag
-                    key={`tag${i}`}
-                    value={oc(tag)("")}
-                    isHighlighted={true}
-                  />
-                ))*/}
+interface IArticleTemplate {
+  data: ArticleTemplateQuery
+}
+const ArticleTemplate: React.FC<IArticleTemplate> = ({ data }) => {
+  return (
+    <AppLayout
+      title={data.markdownRemark?.frontmatter?.title ?? ""}
+      description={data.markdownRemark?.frontmatter?.description ?? ""}>
+      <div className="columns">
+        <div className="column" />
+        <div className="column is-8">
+          <Box>
+            <div className="has-text-centered">
+              <p className="title is-3">
+                {data.markdownRemark?.frontmatter?.title ?? ""}
+              </p>
+              <p className="subtitle is-6">
+                <span className="is-italic">
+                  {data.markdownRemark?.frontmatter?.date ?? ""}
+                </span>
+              </p>
+              <div
+                className="tags are-medium"
+                style={{ justifyContent: "center" }}>
+                {!!data.markdownRemark?.frontmatter?.tags && data.markdownRemark.frontmatter.tags.map((tag, i) => (
+                  <Tag key={`tag${i}`} value={tag ?? ""} isHighlighted />
+                ))}
+              </div>
             </div>
-          </div>
-        </Box>
+          </Box>
+        </div>
+        <div className="column" />
       </div>
-      <div className="column" />
-    </div>
-    <ResponsiveBreakpoint />
-    <div className="columns">
-      <div className="column" />
-      <div className="column is-6">
-        Hello World
-        {/*<div
-          className="content"
-          dangerouslySetInnerHTML={{
-            __html: oc(data).markdownRemark.html(""),
-          }}
-        />*/}
+      <ResponsiveBreakpoint />
+      <div className="columns">
+        <div className="column" />
+        <div className="column is-6">
+          <div
+            className="content"
+            dangerouslySetInnerHTML={{
+              __html: data.markdownRemark?.html ?? ""
+            }}
+          />
+        </div>
+        <div className="column" />
       </div>
-      <div className="column" />
-    </div>
-    <ResponsiveBreakpoint />
-    {/*<BlogPagination next={pageContext.next} previous={pageContext.previous} />*/}
-  </AppLayout>
-)
+      <ResponsiveBreakpoint />
+      {/*<BlogPagination next={pageContext.next} previous={pageContext.previous} />*/}
+    </AppLayout>
+  )
+}
 
 export const PageQuery = graphql`
-  query BlogTemplate($slug: String!) {
+  query ArticleTemplate($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
         description
         tags
         author
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY, MMMM DD")
       }
       html
     }
   }
 `
 
-export default BlogTemplate
+export default ArticleTemplate
