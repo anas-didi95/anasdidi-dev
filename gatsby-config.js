e351436dec11f64773542b35a7e2de7fc0e1551a
Anas Juwaidi <anas.didi95@gmail.com>
2023-09-11 16:12:48 +0800

    MOD: Amend manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,15 +114,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `anasdidi-dev`,
+        short_name: `anasdidi`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
